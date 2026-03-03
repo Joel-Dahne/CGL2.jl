96c903175e1da9f8e4d63d11749e56921bbaec2d
Joel Dahne <joel@dahne.eu>
2026-03-03 14:10:30 -0600

    eigenvalue_solve: Remove CGL2 prefix

--- src/eigenvalue_solve.jl
+++ src/eigenvalue_solve.jl
@@ -17,17 +17,17 @@ function eigenvalue_solve(
     ###
     # Step 1: Compute p_Q
     ###
-    pQ = CGL2.p_Q(γ, κ, ϵ, ξ₁, λ)
+    pQ = p_Q(γ, κ, ϵ, ξ₁, λ)
 
     ###
     # Step 2.1: Solve for γ₁, giving asymptotic behavior of Q_hat at infinity
     ###
     verbose && @info "Solving for γ₁"
 
-    a, b, c = CGL2._abc(κ, ϵ, λ)
+    a, b, c = _abc(κ, ϵ, λ)
     γ₁ = pQ / (-c)^-a
 
-    @assert Arblib.overlaps(CGL2.p_Q_hat(γ₁, κ, ϵ, λ), pQ)
+    @assert Arblib.overlaps(p_Q_hat(γ₁, κ, ϵ, λ), pQ)
 
     verbose && @info "Got" γ₁
 
@@ -36,13 +36,12 @@ function eigenvalue_solve(
     ###
     verbose && @info "Solving for ν and γ₂"
 
-    νF64, γ₂F64 = CGL2.G_hat_approximate(ComplexF64(γ₁), κF64, ϵF64, ξ₁F64, λF64)
+    νF64_approx, γ₂F64_approx = G_hat_approximate(ComplexF64(γ₁), κF64, ϵF64, ξ₁F64, λF64)
 
-    # TODO: Implement rigorous version
-    ν, γ₂ = CGL2.G_hat_solve(Acf(νF64), γ₁, Acf(γ₂F64), κ, ϵ, ξ₁, λ; verbose)
+    ν, γ₂ = G_hat_solve(Acf(νF64_approx), γ₁, Acf(γ₂F64_approx), κ, ϵ, ξ₁, λ; verbose)
 
-    @show ComplexF64(ν) ≈ νF64
-    @show ComplexF64(γ₂) ≈ γ₂F64
+    @show ComplexF64(ν) ≈ νF64_approx
+    @show ComplexF64(γ₂) ≈ γ₂F64_approx
 
     verbose && @info "Got" ν γ₂
 
@@ -58,7 +57,7 @@ function eigenvalue_solve(
 
     λsF64 = filter(
         lambda -> imag(lambda) > 0,
-        CGL2.linearization_eigenvalues_real_1(νF64, κF64, ϵF64, ξ₁F64, λF64)[1],
+        linearization_eigenvalues_real_1(νF64_approx, κF64, ϵF64, ξ₁F64, λF64)[1],
     )
 
     lambdaF64_approx = λsF64[findmax(real, λsF64)[2]]
@@ -92,14 +91,14 @@ function eigenvalue_solve(
     verbose && @info "Solving for λ"
 
     verbose && @info "Solving for λ using midpoint of ν"
-    lambda_mid = CGL2.H_solve(lambda_approx, midpoint(Acb, ν), γ₁, γ₂, κ, ϵ, ξ₁, λ; verbose)
+    lambda_mid = H_solve(lambda_approx, midpoint(Acb, ν), γ₁, γ₂, κ, ϵ, ξ₁, λ; verbose)
 
     # FIXME: Improve enclosures so that we don't need this scaling
-    ν_radius_scaling = Mag(1e-3)
+    ν_radius_scaling = Mag(1e-4)
     verbose && @info "Solving for λ using ν with radius scaled by" ν_radius_scaling
     Arblib.mul!(Arblib.radref(Arblib.realref(ν)), radius(real(ν)), ν_radius_scaling)
     Arblib.mul!(Arblib.radref(Arblib.imagref(ν)), radius(imag(ν)), ν_radius_scaling)
-    lambda = CGL2.H_solve(lambda_approx, ν, γ₁, γ₂, κ, ϵ, ξ₁, λ; verbose)
+    lambda = H_solve(lambda_approx, ν, γ₁, γ₂, κ, ϵ, ξ₁, λ; verbose)
 
     # Note that the finite difference approximation is quite bad
     @show isapprox(ComplexF64(lambda), lambdaF64_approx, rtol = 1e-4)
