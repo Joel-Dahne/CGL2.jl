15831db9db4602d73f2f67f83a3f94d8de502544
Joel Dahne <joel@dahne.eu>
2026-03-13 16:16:55 -0500

    p_Q: Fix that the error was never actually added

--- src/Q_infinity/p_Q.jl
+++ src/Q_infinity/p_Q.jl
@@ -1,20 +1,19 @@
 function p_Q(γ, κ, ϵ, ξ₁, λ::CGLParams{T}) where {T}
-    a, b, c = CGL2._abc(κ, ϵ, λ)
+    a, b, c = _abc(κ, ϵ, λ)
     pQ = c^-a * γ
 
     if T == Arb # Only add error bounds if we are working with an Arb
-        v = Arb("0.1")
+        v = Arb("0.001") # IMPROVE: Can we take v = 0 here?
         (; σ) = λ
 
-        CU = CGL2.UBounds(a, b, c, ξ₁)
-        C = CGL2.FunctionBounds(κ, ϵ, ξ₁, λ, CU)
-        norms = CGL2.NormBounds(γ, κ, ϵ, ξ₁, v, λ, C)
+        # IMPROVE: We need to improve this bound, most likely by
+        # improving C_I_E.
+        CU = UBounds(a, b, c, ξ₁)
+        C = FunctionBounds(κ, ϵ, ξ₁, λ, CU)
+        norms = NormBounds(γ, κ, ϵ, ξ₁, v, λ, C)
         C_p_Q =
-            abs(c^-a) *
-            CGL2.C_I_E(κ, ϵ, ξ₁, v, λ, C) *
-            norms.Q^(2σ + 1) *
-            ξ₁^((2σ + 1) * v - 2)
-        add_error(pQ, C_p_Q)
+            abs(c^-a) * C_I_E(κ, ϵ, ξ₁, v, λ, C) * norms.Q^(2σ + 1) * ξ₁^((2σ + 1) * v - 2)
+        pQ = add_error(pQ, C_p_Q)
     end
 
     return pQ
