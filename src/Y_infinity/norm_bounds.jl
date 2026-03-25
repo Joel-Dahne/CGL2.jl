139eb9733668dcc77165540990c14c5095978cd9
Joel Dahne <joel@dahne.eu>
2026-03-25 16:25:23 -0500

    Y_infinity: Remove norm for Z_dλ

--- src/Y_infinity/norm_bounds.jl
+++ src/Y_infinity/norm_bounds.jl
@@ -1,22 +1,23 @@
 """
     NormBounds_hat(...)
 
-Contains bounds for the norms of `Z` and `Z_dλ`.
+Contains bounds for the norm of `Z`.
 
-The bound for `Z` is based on Lemma REF(lemma:Z-fixed-point-bounds)
-and Proposition REF(prop:Y-fixed-point). The bound for `Z_dλ` is based
-on Lemma REF(lemma:Z-lambda-fixed-point-bounds).
+The bound is based on Lemma REF(lemma:Z-fixed-point-bounds) and
+Proposition REF(prop:Y-fixed-point).
 
 It checks all the conditions on the parameters that these lemmas
 assume. If any of these conditions are not satisfied it will throw an
 error. When using this struct the bounds can therefore safely be
 assume to hold.
+
+We use a struct for this, even though there is only one norm involved,
+to keep the same structure as for `Q` and `Q_hat`.
 """
 struct NormBounds_Y
     Z::Arb
-    Z_dλ::Arb
 
-    NormBounds_Y() = new(indeterminate(Arb), indeterminate(Arb))
+    NormBounds_Y() = new(indeterminate(Arb))
 end
 
 function NormBounds_Y(
@@ -39,16 +40,7 @@ function NormBounds_Y(
     # REF(prop:Z-fixed-point) are checked by the norm_bound_Z
     # function.
 
-    # The requirements for Lemma
-    # REF(lemma:Z-lambda-fixed-point-bounds) that are related to
-    # Lemmas REF(lemma:I_E_hat-I_P_hat-dgamma-bounds) and
-    # REF(lemma:I_K_1_I_K_2-lambda-2-bounds) are checked in the
-    # computation of `C`, where the associated constants are computed.
-    # The other conditions are checked in the norm_bound_Z_dλ
-    # function.
-
     norms.Z[] = norm_bound_Z(c_0, lambda, κ, ϵ, ξ₁, v, λ, C)
-    norms.Z_dλ[] = norm_bound_Z_dλ(c_0, lambda, κ, ϵ, ξ₁, v, λ, C, norms)
 
     return norms
 end
@@ -92,33 +84,5 @@ function norm_bound_Z(
                ξ₁^-v
     else
         throw(ErrorException("could not verify T_12 * ξ₁^-2 < 1"))
-    end
-end
-
-"""
-    norm_bound_Z_dλ(c_0, lambda, κ, ϵ, ξ₁, v, λ, C)
-
-Compute a bound for the norm of `Z_dλ` based on Lemma
-REF(lemma:Z-lambda-fixed-point-bounds).
-"""
-function norm_bound_Z_dλ(
-    c_0::SVector{2,Acb},
-    lambda::Acb,
-    κ::Arb,
-    ϵ::Arb,
-    ξ₁::Arb,
-    v::Arb,
-    λ::CGLParams{Arb},
-    C::FunctionBounds_Y,
-    norms_Z::NormBounds_Y,
-)
-    num =
-        max(C.E_1_dλ * abs(c_0[1]), C.E_2_dλ * abs(c_0[2])) * exp(Arb(-1)) / v +
-        C.Z_dλ_1 * norms_Z.Z
-    den = 1 - C.Z_dλ_2
-    if den > 0
-        return num / den
-    else
-        throw(ErrorException("could not compute bound for norm of Z_dλ"))
     end
 end
