34f00a02b1d7927f728ea7a067a3683cb7e17173
Joel Dahne <joel@dahne.eu>
2026-06-04 10:28:26 -0500

    Y_zero_taylor: Update to match latest version in paper

--- src/Y_zero/Y_taylor.jl
+++ src/Y_zero/Y_taylor.jl
@@ -1,4 +1,63 @@
 """
+    _Y_zero_taylor_remainder_check_conditions(M, N, C, r, a_hat, b_hat, Y1, Y2, κ, ϵ, Λ)
+
+Check that `M`,`N`, `C` and `r` satisfy the conditions of Lemma
+REF(lemma:tail-bound-Y).
+"""
+function _Y_zero_taylor_remainder_check_conditions(
+    M::Int,
+    N::Int,
+    C::Arb,
+    r::Arb,
+    a_hat::ArbSeries,
+    b_hat::ArbSeries,
+    Y1::AcbSeries,
+    Y2::AcbSeries,
+    lambda::Acb,
+    κ::Arb,
+    ϵ::Arb,
+    Λ::CGLParams{Arb},
+)
+    # Ensure the series have coefficient up to N computed
+    @assert Arblib.degree(a_hat) ==
+            Arblib.degree(b_hat) ==
+            Arblib.degree(Y1) ==
+            Arblib.degree(Y2) ==
+            N
+
+    (; d, ω, σ, δ) = Λ
+    @assert isone(σ) # The lemma is only valid for σ = 1
+
+    iseven(N) || return false
+    3M < N || return false
+
+    all(n -> abs(a_hat[n]) <= C * r^n, 0:(M-1)) || return false
+    all(n -> abs(b_hat[n]) <= C * r^n, 0:(M-1)) || return false
+    all(n -> abs(a_hat[n]) <= r^n, M:N) || return false
+    all(n -> abs(b_hat[n]) <= r^n, M:N) || return false
+    all(n -> abs(Y1[n]) <= C * r^n, 0:(M-1)) || return false
+    all(n -> abs(Y2[n]) <= C * r^n, 0:(M-1)) || return false
+    all(n -> abs(Y1[n]) <= r^n, M:N) || return false
+    all(n -> abs(Y2[n]) <= r^n, M:N) || return false
+
+    m = (M + 1) ÷ 2 # This is the same as ceil(M / 2)
+
+    D =
+        (1 + abs(ϵ)) / (1 + ϵ^2) * (
+            abs(κ) / (N + d) +
+            (abs(ω) + abs(lambda)) / ((N + 2) * (N + d)) +
+            6(1 + abs(δ)) * (
+                1 // 8 +
+                1 // 2N +
+                3m * C * ((1 + 3 // N) // 2(N + d)) +
+                3m^2 * C^2 / ((N + 2) * (N + d))
+            )
+        )
+
+    return D <= r^2
+end
+
+"""
     _Y_zero_taylor_remainder(
         Y1::AcbSeries,
         Y2::AcbSeries,
@@ -12,21 +71,8 @@
 
 Compute an enclosure of the remainder term in [`Y_zero_taylor`](@ref).
 
-Let `N` be the degree of the arguments `Y1` and `Y2`. The remainder
-term is bounded by finding `r` such that `abs(Y1[n])` and `abs(Y2[n])`
-are bounded by `r^n` for `n > N`. The remainder term is then bounded
-as
-```
-abs(sum(Y1[n] * ξ₀^n for n = N+1:Inf)) <= (r * ξ₀)^(N + 1) / (1 - r * ξ₀)
-```
-and similarly for `Y2`. For the derivatives we instead get the bound
-```
-abs(sum(n * Y1[n] * ξ₀^(n - 1) for n = N+1:Inf)) <=
-    (r * ξ₀)^N * (N + 1 - N * r * ξ₀) / (1 - r * ξ₀)^2
-```
-and the same for `Y2`.
-
-For details on how we find `r` see lemma:tail-bound-Y in the paper.
+The approach is similar to [`Q_zero_taylor`](@ref) and is based on
+Lemma REF(lemma:tail-bound-Y).
 """
 function _Y_zero_taylor_remainder(
     Y1::AcbSeries,
@@ -40,88 +86,77 @@ function _Y_zero_taylor_remainder(
 )
     @assert Arblib.degree(Y1) == Arblib.degree(Y2)
 
-    indeterminate_result = (indeterminate(κ), indeterminate(κ))
-
-    isfinite(Y1) && isfinite(Y2) || return indeterminate_result
-
     (; d, ω, σ, δ) = Λ
+    N = Arblib.degree(Y1)
 
-    # Compute expansion of forward solution
-    a, b = cgl_equation_real_taylor(
+    isone(σ) || error("No implementation of remainder for σ != 1")
+
+    isfinite(Y1) && isfinite(Y2) || return indeterminate(Arb), indeterminate(Arb)
+
+    # Compute expansion for Q_hat.
+    # We use the fact that the equation is identical to the one for Q,
+    # except for the change in sign for κ and ω.
+    a_hat, b_hat = cgl_equation_real_taylor(
         SVector{2,NTuple{2,Arb}}((real(ν), 0), (imag(ν), 0)),
         -κ,
         ϵ,
         zero(ξ₀),
         CGLParams(Λ, ω = -Λ.ω);
-        degree = Arblib.degree(Y1),
+        degree = N,
     )
 
-    N = Arblib.degree(Y1)
+    # Value of r is a tuning parameter. Lower value gives tighter
+    # enclosures but makes it harder to verify the requirements.
+    r = inv(16ξ₀)
+    @assert 0 < r * ξ₀ < 1 # Sanity check
 
-    if σ == 1
-        # r such that abs(a[n]), abs(b[n]), abs(Y1[n]), abs(Y2[n]) < r^n for n > N
-        r = let
-            # Value of r is a tuning parameter. Lower value gives tighter
-            # enclosures but makes it harder to verify the requirements.
-            r = inv(16ξ₀)
+    # Find C such that abs(a[n]), abs(b[n]) < C * r^n for 0 <= k <= N
+    C = ubound(
+        Arb,
+        1.01max(
+            maximum(n -> abs(a_hat[n] / r^n), 0:N),
+            maximum(n -> abs(b_hat[n] / r^n), 0:N),
+            maximum(n -> abs(Y1[n] / r^n), 0:N),
+            maximum(n -> abs(Y2[n] / r^n), 0:N),
+        ),
+    )
 
-            # Find C such that abs(a[n]), abs(b[n]), abs(Y1[n]), abs(Y2[n]) < C * r^n for 0 <= k <= N
-            C = 1.01max(
-                maximum(n -> abs(a[n] / r^n), 0:N),
-                maximum(n -> abs(b[n] / r^n), 0:N),
-                maximum(n -> abs(Y1[n] / r^n), 0:N),
-                maximum(n -> abs(Y2[n] / r^n), 0:N),
+    M =
+        let M = findlast(
+                n -> !(
+                    abs(a_hat[n]) <= r^n &&
+                    abs(b_hat[n]) <= r^n &&
+                    abs(Y1[n]) <= r^n &&
+                    abs(Y2[n]) <= r^n
+                ),
+                0:N,
             )
-
-            # Find M such that abs(a[n]), abs(b[n]), abs(Y1[n]), abs(Y2[n]) <= r^n for M <= n <= N
-            M =
-                let M = findlast(
-                        n -> !(
-                            abs(a[n]) <= r^n &&
-                            abs(b[n]) <= r^n &&
-                            abs(Y1[n]) <= r^n &&
-                            abs(Y2[n]) <= r^n
-                        ),
-                        0:N,
-                    )
-                    isnothing(M) ? 0 : M
-                end
-
-            # Verify that r, C and M satisfy the requirements
-            all(n -> abs(a[n]) <= C * r^n, 0:(M-1)) || return indeterminate_result
-            all(n -> abs(a[n]) <= r^n, M:N) || return indeterminate_result
-            all(n -> abs(b[n]) <= C * r^n, 0:(M-1)) || return indeterminate_result
-            all(n -> abs(b[n]) <= r^n, M:N) || return indeterminate_result
-            all(n -> abs(Y1[n]) <= C * r^n, 0:(M-1)) || return indeterminate_result
-            all(n -> abs(Y1[n]) <= r^n, M:N) || return indeterminate_result
-            all(n -> abs(Y2[n]) <= C * r^n, 0:(M-1)) || return indeterminate_result
-            all(n -> abs(Y2[n]) <= r^n, M:N) || return indeterminate_result
-
-            # This is needed for the lemma to apply
-            3M < N || return indeterminate_result
-
-            D =
-                (1 + abs(ϵ)) / (1 + ϵ^2) * (
-                    abs(κ) / (N + d) +
-                    (abs(κ / σ) + abs(lambda) + abs(ω)) / ((N + 2) * (N + d)) +
-                    3(1 + abs(δ)) * (1 + 6M * C^3 / (N + d))
-                )
-
-            D <= r^2 || return indeterminate_result
-
-            r
+            isnothing(M) ? 0 : M
         end
 
-        @assert 0 < r * ξ₀ < 1
-
-        remainder_bound = (r * ξ₀)^(N + 1) / (1 - r * ξ₀)
-        remainder_derivative_bound = (r * ξ₀)^N * (N + 1 - N * r * ξ₀) / (1 - r * ξ₀)^2
-
-        remainder = add_error(Acb(0), remainder_bound)
-        remainder_derivative = add_error(Acb(0), remainder_derivative_bound)
-    else
-        error("No implementation of remainder for σ != 1")
+    # Check that the conditions of the lemma are satisfied
+    if !_Y_zero_taylor_remainder_check_conditions(
+        M,
+        N,
+        C,
+        r,
+        a_hat,
+        b_hat,
+        Y1,
+        Y2,
+        lambda,
+        κ,
+        ϵ,
+        Λ,
+    )
+        return indeterminate(Arb), indeterminate(Arb)
     end
+
+    remainder_bound = (r * ξ₀)^(N + 1) / (1 - r * ξ₀)
+    remainder_derivative_bound = (r * ξ₀)^N * (N + 1 - N * r * ξ₀) / (1 - r * ξ₀)^2
+
+    remainder = add_error(Arb(0), remainder_bound)
+    remainder_derivative = add_error(Arb(0), remainder_derivative_bound)
 
     return remainder, remainder_derivative
 end
@@ -154,7 +189,6 @@ function Y_zero_taylor(
         ν,
         κ,
         ϵ,
-        zero(ξ₀),
         Λ;
         degree,
     )
--- src/Y_zero/equation.jl
+++ src/Y_zero/equation.jl
@@ -45,127 +45,13 @@ end
 cgl_linearization_equation(u, (lambda, κ, ϵ, Q_hat, Λ), ξ) =
     cgl_linearization_equation(u, lambda, κ, ϵ, ξ, Q_hat, Λ)
 
-function cgl_linearization_equation_real(
-    YZ_reim,
-    lambda_real,
-    lambda_imag,
-    κ,
-    ϵ,
-    ξ,
-    Q_hat,
-    Λ::CGLParams,
-)
-    (; d, ω, σ, δ) = Λ
-    Y_r = SVector(YZ_reim[1], YZ_reim[2])
-    Y_i = SVector(YZ_reim[3], YZ_reim[4])
-    Z_r = SVector(YZ_reim[5], YZ_reim[6])
-    Z_i = SVector(YZ_reim[7], YZ_reim[8])
+"""
+    _cgl_linearization_equation_taylor_J_N_taylor(ν, κ, ϵ, ξ₀, Λ; degree)
 
-    a, b = Q_hat(ξ)
-    N₁_a = -(a^2 + b^2)^(σ - 1) * (δ * (1 + 2σ) * a^2 + 2σ * a * b + δ * b^2)
-    N₁_b = -(a^2 + b^2)^(σ - 1) * (a^2 + 2δ * σ * a * b + (1 + 2σ) * b^2)
-    N₂_a = (a^2 + b^2)^(σ - 1) * ((1 + 2σ) * a^2 - 2δ * σ * a * b + b^2)
-    N₂_b = -(a^2 + b^2)^(σ - 1) * (δ * a^2 - 2σ * a * b + δ * (1 + 2σ) * b^2)
-
-    # Compute the matrices M1 and M2 following same approach as in CAPD
-    begin
-        M1_11_real = (-ϵ * (κ / σ + N₁_a - lambda_real) - N₂_a - ω) / (1 + ϵ^2)
-        M1_11_imag = ϵ * lambda_imag / (1 + ϵ^2)
-        M1_12_real = (-κ / σ - ϵ * (N₁_b - ω) - N₂_b + lambda_real) / (1 + ϵ^2)
-        M1_12_imag = lambda_imag / (1 + ϵ^2)
-        M1_21_real = (κ / σ - ϵ * (N₂_a + ω) + N₁_a - lambda_real) / (1 + ϵ^2)
-        M1_21_imag = -lambda_imag / (1 + ϵ^2)
-        M1_22_real = (-ϵ * (κ / σ + N₂_b - lambda_real) + N₁_b - ω) / (1 + ϵ^2)
-        M1_22_imag = ϵ * lambda_imag / (1 + ϵ^2)
-
-        M2_11 = κ / (1 + ϵ^2) * (-ϵ) * ξ - (d - 1) / ξ
-        M2_12 = κ / (1 + ϵ^2) * (-1) * ξ
-        M2_21 = -M2_12
-        M2_22 = M2_11
-
-        M1_real_cpp = @SMatrix[M1_11_real M1_12_real; M1_21_real M1_22_real]
-        M1_imag_cpp = @SMatrix[M1_11_imag M1_12_imag; M1_21_imag M1_22_imag]
-        M2_cpp = @SMatrix[M2_11 M2_12; M2_21 M2_22]
-    end
-
-    # Compute them using complex matrix arithmetic
-    begin
-        lambda = complex(lambda_real, lambda_imag)
-
-        A_inv = @SMatrix[ϵ 1; -1 ϵ] / (1 + ϵ^2)
-        B₁ = @SMatrix[κ 0; 0 κ]
-        #B₂ = (d - 1) * @SMatrix[ϵ -1; 1 ϵ]
-
-        C = @SMatrix[κ/σ -ω; ω κ/σ]
-
-        J_N = @SMatrix[N₁_a N₁_b; N₂_a N₂_b]
-
-        M1 = -A_inv * (C + J_N - lambda * I)
-        M2 = -(A_inv * B₁ * ξ + (d - 1) * I / ξ)
-    end
-    # Check that they agree
-    # TODO: Only compute these things once
-    @assert real(M1) ≈ M1_real_cpp
-    @assert imag(M1) ≈ M1_imag_cpp
-    if !iszero(ξ)
-        @assert M2 ≈ M2_cpp
-    end
-
-    # Compute components directly with same approach as in CAPD
-    begin
-        Y_r_1, Y_r_2 = Y_r
-        Y_i_1, Y_i_2 = Y_i
-        Z_r_1, Z_r_2 = Z_r
-        Z_i_1, Z_i_2 = Z_i
-        dZ_r_1 =
-            M1_11_real * Y_r_1 + M1_12_real * Y_r_2 -
-            (M1_11_imag * Y_i_1 + M1_12_imag * Y_i_2)
-        dZ_r_2 =
-            M1_21_real * Y_r_1 + M1_22_real * Y_r_2 -
-            (M1_21_imag * Y_i_1 + M1_22_imag * Y_i_2)
-        dZ_i_1 =
-            M1_11_imag * Y_r_1 +
-            M1_12_imag * Y_r_2 +
-            M1_11_real * Y_i_1 +
-            M1_12_real * Y_i_2
-        dZ_i_2 =
-            M1_21_imag * Y_r_1 +
-            M1_22_imag * Y_r_2 +
-            M1_21_real * Y_i_1 +
-            M1_22_real * Y_i_2
-        if iszero(ξ)
-            @assert iszero(Z_r_1) && iszero(Z_r_2) && iszero(Z_i_1) && iszero(Z_i_2)
-        else
-            dZ_r_1 += M2_11 * Z_r_1 + M2_12 * Z_r_2
-            dZ_r_2 += M2_21 * Z_r_1 + M2_22 * Z_r_2
-            dZ_i_1 += M2_11 * Z_i_1 + M2_12 * Z_i_2
-            dZ_i_2 += M2_21 * Z_i_1 + M2_22 * Z_i_2
-        end
-    end
-
-    # Compute using matrix arithmetic
-    dY_r = Z_r
-    dY_i = Z_i
-    dZ_r = real(M1) * Y_r - imag(M1) * Y_i
-    dZ_i = imag(M1) * Y_r + real(M1) * Y_i
-    if iszero(ξ)
-        @assert iszero(Z_r) && iszero(Z_i)
-    else
-        dZ_r += M2 * Z_r
-        dZ_i += M2 * Z_i
-    end
-
-    # Check that they agree
-    @assert dZ_r ≈ SVector(dZ_r_1, dZ_r_2)
-    @assert dZ_i ≈ SVector(dZ_i_1, dZ_i_2)
-
-    return vcat(dY_r, dY_i, dZ_r, dZ_i)
-end
-
-# For use with ODEProblem
-cgl_linearization_equation_real(u, (lambda_real, lambda_imag, κ, ϵ, Q_hat, Λ), ξ) =
-    cgl_linearization_equation_real(u, lambda_real, lambda_imag, κ, ϵ, ξ, Q_hat, Λ)
-
+Compute a Taylor expansion of the term `J_N` in the equation for the
+linear equation. It is done by computing the Taylor expansions of the
+forward solution `Q_hat` and putting together `J_N` from this.
+"""
 function _cgl_linearization_equation_taylor_J_N_taylor(
     ν::Acb,
     κ::Arb,
@@ -176,7 +62,10 @@ function _cgl_linearization_equation_taylor_J_N_taylor(
 )
     (; d, ω, σ, δ) = Λ
 
-    a, b = cgl_equation_real_taylor(
+    # Compute expansion for Q_hat.
+    # We use the fact that the equation is identical to the one for Q,
+    # except for the change in sign for κ and ω.
+    a_hat, b_hat = cgl_equation_real_taylor(
         SVector{2,NTuple{2,Arb}}((real(ν), 0), (imag(ν), 0)),
         -κ,
         ϵ,
@@ -185,55 +74,58 @@ function _cgl_linearization_equation_taylor_J_N_taylor(
         degree,
     )
 
-    N₁_a = -(a^2 + b^2)^(σ - 1) * (δ * (1 + 2σ) * a^2 + 2σ * a * b + δ * b^2)
-    N₁_b = -(a^2 + b^2)^(σ - 1) * (a^2 + 2δ * σ * a * b + (1 + 2σ) * b^2)
-    N₂_a = (a^2 + b^2)^(σ - 1) * ((1 + 2σ) * a^2 - 2δ * σ * a * b + b^2)
-    N₂_b = -(a^2 + b^2)^(σ - 1) * (δ * a^2 - 2σ * a * b + δ * (1 + 2σ) * b^2)
+    J_N_11 =
+        -(a_hat^2 + b_hat^2)^(σ - 1) *
+        (δ * (1 + 2σ) * a_hat^2 + 2σ * a_hat * b_hat + δ * b_hat^2)
+    J_N_12 =
+        -(a_hat^2 + b_hat^2)^(σ - 1) *
+        (a_hat^2 + 2δ * σ * a_hat * b_hat + (1 + 2σ) * b_hat^2)
+    J_N_21 =
+        (a_hat^2 + b_hat^2)^(σ - 1) *
+        ((1 + 2σ) * a_hat^2 - 2δ * σ * a_hat * b_hat + b_hat^2)
+    J_N_22 =
+        -(a_hat^2 + b_hat^2)^(σ - 1) *
+        (δ * a_hat^2 - 2σ * a_hat * b_hat + δ * (1 + 2σ) * b_hat^2)
 
-    return @SMatrix[N₁_a N₁_b; N₂_a N₂_b]
+    return @SMatrix[J_N_11 J_N_12; J_N_21 J_N_22]
 end
 
+"""
+    cgl_linearization_equation_taylor(Y_0, lambda, ν, κ, ϵ, Λ; degree)
+
+Compute the Taylor expansions of `Y`. The expansion is centered at the
+point `ξ = 0`, with the leading coefficients given by `Y_0` and the
+linear are zero.
+"""
 function cgl_linearization_equation_taylor(
-    Y_ξ₀::SVector{2,NTuple{2,Acb}},
+    Y_0::SVector{2,NTuple{2,Acb}},
     lambda::Acb,
     ν::Acb,
     κ::Arb,
     ϵ::Arb,
-    ξ₀::Arb,
     Λ::CGLParams{Arb};
     degree::Integer = 5,
 )
     (; d, ω, σ, δ) = Λ
 
-    Y1 = AcbSeries(Y_ξ₀[1]; degree)
-    Y2 = AcbSeries(Y_ξ₀[2]; degree)
+    Y1 = AcbSeries(Y_0[1]; degree)
+    Y2 = AcbSeries(Y_0[2]; degree)
 
-    if iszero(ξ₀) && !isone(d) && !iszero(Y1[1]) && !iszero(Y2[1])
-        return SVector(indeterminate(Y1), indeterminate(Y2))
-    end
-
-    A = SMatrix{2,2}(ϵ, 1, -1, ϵ)
-    B₁ = SMatrix{2,2}(κ, 0, 0, κ)
-    B₂ = (d - 1) * A
-    C = SMatrix{2,2}(κ / σ, ω, -ω, κ / σ)
-    J_N = _cgl_linearization_equation_taylor_J_N_taylor(ν, κ, ϵ, ξ₀, Λ; degree)
+    J_N = _cgl_linearization_equation_taylor_J_N_taylor(ν, κ, ϵ, Arb(0), Λ; degree)
 
     for n = 0:2:(degree-2)
-        if iszero(ξ₀)
-            v = J_N * SVector(AcbSeries(Y1, degree = n + 1), AcbSeries(Y2, degree = n + 1))
+        v = J_N * SVector(AcbSeries(Y1, degree = n + 1), AcbSeries(Y2, degree = n + 1))
 
-            inv_rhs = @SMatrix[ϵ 1; -1 ϵ] / ((n + 2) * (n + d) * (1 + ϵ^2))
-            M =
-                @SMatrix[
-                    (ϵ * (n * κ + κ / σ - lambda) + ω) (n * κ + κ / σ - lambda - ϵ * ω);
-                    (-n * κ - κ / σ + lambda + ϵ * ω) (ϵ * (n * κ + κ / σ - lambda) + ω)
-                ] / ((n + 2) * (n + d) * (1 + ϵ^2))
+        # Explicit inverse of (n + 2) * ((n + 1) * A + B₂)
+        inv_rhs = @SMatrix[ϵ 1; -1 ϵ] / ((n + 2) * (n + d) * (1 + ϵ^2))
+        # Explicit value for above inverse multiplied with n * B₁ + C - λ * I
+        M =
+            @SMatrix[
+                (ϵ * (n * κ + κ / σ - lambda) + ω) (n * κ + κ / σ - lambda - ϵ * ω);
+                (-n * κ - κ / σ + lambda + ϵ * ω) (ϵ * (n * κ + κ / σ - lambda) + ω)
+            ] / ((n + 2) * (n + d) * (1 + ϵ^2))
 
-            Y1[n+2], Y2[n+2] =
-                -M * SVector(Y1[n], Y2[n]) - inv_rhs * SVector(v[1][n], v[2][n])
-        else
-            error("case ξ₀ != 0 not implemented")
-        end
+        Y1[n+2], Y2[n+2] = -M * SVector(Y1[n], Y2[n]) - inv_rhs * SVector(v[1][n], v[2][n])
     end
 
     return SVector(Y1, Y2)
