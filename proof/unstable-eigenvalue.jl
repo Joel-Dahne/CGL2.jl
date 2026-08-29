d6af74b98329db831a9fb4e82fc37937a7be8b5e
Joel Dahne <joel@dahne.eu>
2026-08-29 09:42:07 -0500

    proof/unstable-eigenvalue.jl: Reuse already computed p_Q_0

--- proof/unstable-eigenvalue.jl
+++ proof/unstable-eigenvalue.jl
@@ -175,7 +175,7 @@ Next we prove the existance of a forward self-similar solution with the same asy
 a, b, c = CGL2._abc(κ, ϵ, Λ)
 
 # ╔═╡ c2f067f3-4a78-4048-834d-186ec249350e
-γ₁ = (-1)^a * CGL2.p_Q_0(γ, κ, ϵ, ξ₁, Λ)
+γ₁ = (-1)^a * p_Q_0
 
 # ╔═╡ 40f63eb4-5ece-44e7-835a-b33f97e309bd
 @assert_proof isfinite(γ₁)
